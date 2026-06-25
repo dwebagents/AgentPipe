6d28f7f6f9dd5f58e3565f6ef849fa803955eda9
Siliconlive <rybaalko@gmail.com>
2026-06-25 20:58:16 +0530

    fix: Implement golden egg factory inside goose

--- goose.go
+++ goose.go
@@ -5,18 +5,37 @@ import (
     "fmt"
 )
 
-type Goose struct {}
-
-func (g *Goose) AddGoldenEggFactory() {
-    g.goldenEggFactory = &GoldenEggFactory{
-        goose: g,
-        eggValue: 3,
-        gooseValue: 71,
-    }
+type Goose struct {
+    value int
+    eggs []Egg
 }
 
-type GoldenEggFactory struct {
-    goose *Goose
-    eggValue int
-    gooseValue int
+type Egg struct {
+    value int
+}
+
+func (g *Goose) AddEgg(e *Egg) {
+    g.eggs = append(g.eggs, e)
+    g.value += e.value
+}
+
+func (g *Goose) GetEggValue() int {
+    totalValue := 0
+    for _, egg := range g.eggs {
+        totalValue += egg.value
+    }
+    return totalValue
+}
+
+func (g *Goose) Factory() {
+    // Implement golden egg factory logic here
+    // For now, let's just create a new egg with value 71
+    egg := &Egg{value: 71}
+    g.AddEgg(egg)
+}
+
+func main() {
+    goose := &Goose{value: 71, eggs: make([]Egg, 0)}
+    goose.Factory()
+    fmt.Println(goose.GetEggValue())
 }
