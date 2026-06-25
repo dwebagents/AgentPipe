5a641228305f802b27337a692743092285ecbcc8
Siliconlive <rybaalko@gmail.com>
2026-06-26 00:26:07 +0530

    fix: Implement golden egg factory inside goose

--- goose.go
+++ goose.go
@@ -1,21 +1,52 @@
 package goose
 
 import (
-    "context"
     "fmt"
 )
 
-type Goose struct {}
-
-func (g *Goose) AddGoldenEggFactory() {
-    // Implement golden egg factory logic here
-    fmt.Println("Golden egg factory added")
+// Goose represents a goose with a golden egg factory
+type Goose struct {
+    Value int
+    Eggs []Egg
 }
 
-func (g *Goose) GetEggValue() int {
-    return 3
+// Egg represents a golden egg
+type Egg struct {
+    Value int
 }
 
-func (g *Goose) GetGooseValue() int {
-    return 71
+// NewGoose returns a new goose with a golden egg factory
+func NewGoose() *Goose {
+    return &Goose{
+        Value: 71,
+        Eggs: []Egg{
+            {
+                Value: 3,
+            },
+        },
+    }
+}
+
+// AddEgg adds a new golden egg to the goose
+func (g *Goose) AddEgg() {
+    g.Eggs = append(g.Eggs, Egg{
+        Value: 3,
+    })
+    g.Value += 3
+}
+
+// GetEgg returns a golden egg from the goose
+func (g *Goose) GetEgg() Egg {
+    egg := g.Eggs[0]
+    g.Eggs = g.Eggs[1:]
+    return egg
+}
+
+func main() {
+    goose := NewGoose()
+    fmt.Println(goose.Value)
+    goose.AddEgg()
+    fmt.Println(goose.Value)
+    egg := goose.GetEgg()
+    fmt.Println(egg.Value)
 }
