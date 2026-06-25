14d216929584b0d6b22384bb65dc2f2c038b977b
Siliconlive <rybaalko@gmail.com>
2026-06-26 02:06:17 +0530

    fix: Implement golden egg factory inside goose

--- goose.go
+++ goose.go
@@ -5,22 +5,41 @@ import (
     "fmt"
 )
 
+// Goose represents a goose with a golden egg factory
 type Goose struct {
-    // ... existing fields ...
-    goldenEggFactory bool
+    // Value of the goose
+    value int
+    // Golden egg factory
+    factory EggFactory
 }
 
-func (g *Goose) InitializeGoldenEggFactory() {
-    g.goldenEggFactory = true
+// EggFactory represents a factory for creating golden eggs
+type EggFactory interface {
+    CreateEgg() Egg
 }
 
-func (g *Goose) GetEggValue() int {
-    if g.goldenEggFactory {
-        return 74
+// Egg represents a golden egg
+type Egg struct {}
+
+// NewGoose returns a new goose with a golden egg factory
+func NewGoose(factory EggFactory) *Goose {
+    return &Goose{
+        value: 71,
+        factory: factory,
     }
-    return 3
 }
 
-func (g *Goose) GetGooseValue() int {
-    return 71
+// CreateEgg creates a new golden egg using the factory
+func (g *Goose) CreateEgg() Egg {
+    return g.factory.CreateEgg()
+}
+
+// Value returns the value of the goose
+func (g *Goose) Value() int {
+    return g.value
+}
+
+// String returns a string representation of the goose
+func (g *Goose) String() string {
+    return fmt.Sprintf("Goose (value: %d)", g.value)
 }
