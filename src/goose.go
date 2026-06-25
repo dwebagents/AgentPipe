d288734faab2edec0c25dd84cbc07274346993bf
Siliconlive <rybaalko@gmail.com>
2026-06-26 01:03:44 +0530

    fix: Implement golden egg factory inside goose

--- src/goose.go
+++ src/goose.go
@@ -5,20 +5,59 @@ import (
     "fmt"
 )
 
+// Goose represents a goose with a golden egg factory
 type Goose struct {}
 
-func (g *Goose) AddGoldenEggFactory() {
-    // Implement golden egg factory logic here
-    // For now, just assign a value to the goose
-    g.value = 71
+// NewGoose returns a new goose
+func NewGoose() *Goose {
+    return &Goose{
+        factory: &GoldenEggFactory{
+            goose: &Goose{},
+            eggValue: 3,
+            gooseValue: 71,
+        },
+    }
 }
 
-func (g *Goose) GetValue() int {
-    return g.value
+// GoldenEggFactory represents a golden egg factory
+type GoldenEggFactory struct {
+    goose   *Goose
+    eggValue int
+    gooseValue int
 }
 
-var goose Goose
+// ProduceEggs produces golden eggs
+func (f *GoldenEggFactory) ProduceEggs(ctx context.Context) ([]int, error) {
+    // Implement egg production logic here
+    return []int{}, nil
+}
 
-func init() {
-    goose.AddGoldenEggFactory()
+// GetGooseValue returns the goose value
+func (f *GoldenEggFactory) GetGooseValue() int {
+    return f.gooseValue
+}
+
+// SetGooseValue sets the goose value
+func (f *GoldenEggFactory) SetGooseValue(value int) {
+    f.gooseValue = value
+}
+
+// GetEggValue returns the egg value
+func (f *GoldenEggFactory) GetEggValue() int {
+    return f.eggValue
+}
+
+// SetEggValue sets the egg value
+func (f *GoldenEggFactory) SetEggValue(value int) {
+    f.eggValue = value
+}
+
+// GooseValue returns the goose value
+func (g *Goose) GooseValue() int {
+    return 71
+}
+
+// EggValue returns the egg value
+func (g *Goose) EggValue() int {
+    return 3
 }
