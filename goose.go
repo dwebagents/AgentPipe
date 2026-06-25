3f476caa13ea2cef77c6f0404574ef3dea0a1c88
Siliconlive <rybaalko@gmail.com>
2026-06-25 21:17:09 +0530

    fix: Implement golden egg factory inside goose

--- goose.go
+++ goose.go
@@ -8,14 +8,13 @@ import (
 type Goose struct {}
 
 func (g *Goose) AddGoldenEggFactory() {
-    // Calculate new goose value based on whitepaper
-    g.value = 71
-    // Implement golden egg factory
-    g.eggFactory = &eggFactory{
+    g.goldenEggFactory = &goldenEggFactory{
         eggValue: 3,
+        gooseValue: 71,
     }
 }
 
-type eggFactory struct {
+type goldenEggFactory struct {
     eggValue int
+    gooseValue int
 }
