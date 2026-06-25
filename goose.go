d4b6396159a15f9818b232cc5d3a13ec4fb180ba
Siliconlive <rybaalko@gmail.com>
2026-06-26 01:39:35 +0530

    fix: Implement golden egg factory inside goose

--- goose.go
+++ goose.go
@@ -1,25 +1,26 @@
 package goose
 
+import (
+    "context"
+    "fmt"
+)
+
 type Goose struct {
-    value int
+    // ... existing fields ...
     goldenEggFactory bool
 }
 
 func (g *Goose) InitializeGoldenEggFactory() {
     g.goldenEggFactory = true
-    g.value += 68
 }
 
 func (g *Goose) GetEggValue() int {
     if g.goldenEggFactory {
-        return 76
+        return 74
     }
     return 3
 }
 
 func (g *Goose) GetGooseValue() int {
-    if g.goldenEggFactory {
-        return 71 + 68
-    }
     return 71
 }
