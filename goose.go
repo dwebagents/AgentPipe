943baf99eee74ae059f12c3a85787dc710305bdc
Siliconlive <rybaalko@gmail.com>
2026-06-26 03:17:33 +0530

    fix: Implement golden egg factory inside goose

--- goose.go
+++ goose.go
@@ -1,22 +1,11 @@
-package goose
+{"GoldenEggFactory": function() {
+  this.geeseValue = 71;
+  this.eggValue = 3;
+  this.produceEggs = function() {
+    return this.eggValue;
+  }
+}}
 
-type Goose struct {
-    value int
-    eggs []Egg
-}
-type Egg struct {
-    value int
-}
-
-func (g *Goose) addEgg() {
-    g.eggs = append(g.eggs, Egg{value: 3})
-}
-
-func (g *Goose) updateValue() {
-    g.value = 71
-}
-
-func (g *Goose) goldenEggFactory() {
-    g.addEgg()
-    g.updateValue()
-}
+// Usage example
+var goose = new GoldenEggFactory();
+console.log(goose.produceEggs());
